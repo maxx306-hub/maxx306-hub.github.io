3e74a51933efd6d1c31c3df06c66e7bf8f990acd
maxx306-hub <77285194+maxx306-hub@users.noreply.github.com>
2021-03-15 11:44:28 +0200

    Add files via upload

---
+++ Vertolotik/main2.js
@@ -0,0 +1,95 @@
+//SHOW MORE34
+const read__more4 = document.querySelector('.show__more4');
+const list4 = document.querySelector('.hidden__games4');
+
+read__more4.addEventListener("click",(e)=>{
+  list4.classList.toggle("ddd4");
+  read__more4.classList.toggle("activee4");
+  if(read__more4.innerText === "Show more"){
+    read__more4.innerText = "Show less";
+  }else {
+    read__more4.innerText = "Show more";
+  }
+});
+
+
+const checkbox1 = document.querySelector('.checkbox1');
+
+checkbox1.addEventListener("click", (e) =>{
+    checkbox1.classList.toggle("checked1");
+    if(checkbox1.innerText === "✔"){
+      checkbox1.innerText = " ";
+    }else {
+      checkbox1.innerText = "✔";
+    }
+})
+const checkbox2 = document.querySelector('.checkbox2');
+
+checkbox2.addEventListener("click", (e) =>{
+    checkbox2.classList.toggle("checked2");
+    if(checkbox2.innerText === "✔"){
+      checkbox2.innerText = " ";
+    }else {
+      checkbox2.innerText = "✔";
+    }
+})
+const checkbox3 = document.querySelector('.checkbox3');
+
+checkbox3.addEventListener("click", (e) =>{
+    checkbox3.classList.toggle("checked3");
+    if(checkbox3.innerText === "✔"){
+      checkbox3.innerText = " ";
+    }else {
+      checkbox3.innerText = "✔";
+    }
+})
+const checkbox4 = document.querySelector('.checkbox4');
+
+checkbox4.addEventListener("click", (e) =>{
+    checkbox4.classList.toggle("checked4");
+    if(checkbox4.innerText === "✔"){
+      checkbox4.innerText = " ";
+    }else {
+      checkbox4.innerText = "✔";
+    }
+})
+const checkbox5 = document.querySelector('.checkbox5');
+
+checkbox5.addEventListener("click", (e) =>{
+    checkbox5.classList.toggle("checked5");
+    if(checkbox5.innerText === "✔"){
+      checkbox5.innerText = " ";
+    }else {
+      checkbox5.innerText = "✔";
+    }
+})
+const checkbox6 = document.querySelector('.checkbox6');
+
+checkbox6.addEventListener("click", (e) =>{
+    checkbox6.classList.toggle("checked6");
+    if(checkbox6.innerText === "✔"){
+      checkbox6.innerText = " ";
+    }else {
+      checkbox6.innerText = "✔";
+    }
+})
+const checkbox7 = document.querySelector('.checkbox7');
+
+checkbox7.addEventListener("click", (e) =>{
+    checkbox7.classList.toggle("checked7");
+    if(checkbox7.innerText === "✔"){
+      checkbox7.innerText = " ";
+    }else {
+      checkbox7.innerText = "✔";
+    }
+})
+const checkbox8 = document.querySelector('.checkbox8');
+
+checkbox8.addEventListener("click", (e) =>{
+    checkbox8.classList.toggle("checked8");
+    if(checkbox8.innerText === "✔"){
+      checkbox8.innerText = " ";
+    }else {
+      checkbox8.innerText = "✔";
+    }
+})
